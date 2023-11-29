aec9cf93e2c1184bb9eeca46bb98fcc4dde0a540
Muees99 <132849693+Muees99@users.noreply.github.com>
2023-11-29 20:33:41 +0100

    useParams to Fetch Ingredients Recipe

--- src/pages/recipe/Recipes.js
+++ src/pages/recipe/Recipes.js
@@ -1,9 +1,30 @@
+import { useParams} from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Recipe.css'
 
 import React from 'react'
 
 export default function Recipe() {
+  const{id} = useParams()
+  const url='http://localhost:3000/recipes/'+id 
+  const{error,isPending ,data:recipe}=useFetch(url)
+
   return (
-    <div>Recipes</div>
+    <div className='recipes'>
+      {error && <p>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+        <h2 className='page-title'>{recipe.title}</h2>
+        <p>Takes{recipe.cookingTime} to cook.</p>
+        <ul>
+          {recipe.ingredients.map(ingredient=><li key={ingredient}>{ingredient}</li>)}
+          <p className='method'>{recipe.method}</p>
+        </ul>
+        </>
+
+      )}
+      
+    </div>
   )
 }
